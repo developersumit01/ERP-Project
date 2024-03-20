7099341e20bacd94518e4d75c872873df4fd1dcd
Sumit Kumar <sumitkumar84390@gmail.com>
2024-03-20 20:00:13 +0530

    Make the component of time table

--- src/main/webapp/src/Components/WeekDays.jsx
+++ src/main/webapp/src/Components/WeekDays.jsx
@@ -1,14 +1,31 @@
-import CalenderCSS from '../CSS/Calender.module.css';
+import { useState } from "react";
+import CalenderCSS from "../CSS/Calender.module.css";
 
 const WeekDays = () => {
   const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const handleClick = (e) => {
+    console.log(e);
+    setMarker(e.target.id);
+  };
+  const [marker, setMarker] = useState(new Date().getDay());
   return (
-      <div className={CalenderCSS.weakDays}>
-      {days.map((ele) => {
-        return <div className={CalenderCSS.day}>{ele}</div>;
+    <div className={CalenderCSS.weakDays}>
+      {days.map((ele, index) => {
+        return (
+          <div className={CalenderCSS.day}>
+            <input
+              type="radio"
+              id={index}
+              name="day"
+              onClick={handleClick}
+              checked={index == marker}
+            />
+            <label htmlFor={index}>{ele}</label>
+          </div>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default WeekDays
+export default WeekDays;
